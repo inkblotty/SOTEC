1d266ec02be962b5bb915d5bc82bb65dab839a8a
inkblotty <inkblot.and.blush@gmail.com>
2016-01-31 04:08:28 -0700

    created new scss file for media queries and @include in main.scss

--- README.md
+++ README.md
@@ -4,14 +4,9 @@ SOTEC website redesign
 - General
   * mobile friendly
   * make staff section visible on menu click
-  * new section: Stuff we Do Together
 
 - Stuff We Do Together
   * banner for special events
 
-- Theology Section
+- About us Section
   * updated content?
-
-- Calendar
-  * bottom cuts off :c
-  * won't update Saturday
--- src/scripts/services/calendarService.js
+++ src/scripts/services/calendarService.js
@@ -4,7 +4,6 @@ app.factory('calendarServ', ['$http', function($http) {
   
   // https://www.googleapis.com/calendar/v3/calendars/scumbuilding%40gmail.com/events?timeMin=timeMin=2016-01-28T08:10:18.967Z&singleEvents=true&orderBy=startTime&key=AIzaSyDtk9bOYs6qOGL-jILsBMZVm2jlX9x9ats";
 
-
   return $http.get(url)
     .success(function(data) {
       return data;
