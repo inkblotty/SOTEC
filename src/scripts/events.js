41289965ab0ccc2fc46467cfed6bcbc71817848d
inkblotty <inkblot.and.blush@gmail.com>
2016-02-03 13:02:57 -0700

    changed animations for accordion effect; removed angular animations in exchange for jquery

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -8,11 +8,23 @@ $("a[href^='#']").click(function(e) {
 		scrollTop: $(dest).offset().top-10}, 'slow');
 });
 
+/* ACCORDION INFO EVENTS */
+// sets accord-info to display: none on page load
+$('.accord-info').css('display', 'none');
+
+/*
 $('.expand-arrow').click(function(e) {
 	$(this).toggleClass('active');
 	e.preventDefault();
+});*/
+
+$('.info-title').click(function(e) {
+	e.preventDefault();
+	$(this).find('.expand-arrow').toggleClass('active');
+	$(this).siblings('.accord-info').slideToggle();
 });
 
+// Calendar Events
 $('#full-cal-link').click(function(e) {
 	$('#calendar-popOver').toggleClass('hidden');
 });
