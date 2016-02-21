e895527b2252b34ee4949b349a25f10c18b6694b
inkblotty <inkblot.and.blush@gmail.com>
2016-02-20 22:55:13 -0700

    added footer with link to portfolio; styling for footer; added jquery detect for iphones and set background-image:none until background position issue is fixed; made sure backgrounds are black

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -3,6 +3,10 @@ $('#no-JS').attr('height', '0');
 $('#no-JS').attr('width', '0');
 $('#no-JS').attr('overflow', 'hidden');
 
+if (navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)) {
+	$('body').css('background-image', 'none');
+}
+
 /* MENU EVENTS */
 
 $("a[href^='#']").click(function(e) {
